2963b128f5dfc0c0dba0151a4cb71ce53cb61b18
Jachin <82848023+wardaddy9@users.noreply.github.com>
2023-06-09 14:47:59 -0500

    Create main.tf
    https://registry.terraform.io/modules/terraform-google-modules/kubernetes-engine/google/latest
    
    gke cluster config

---
+++ main.tf
@@ -0,0 +1,91 @@
+#https://registry.terraform.io/modules/terraform-google-modules/kubernetes-engine/google/latest
+# google_client_config and kubernetes provider must be explicitly specified like the following.
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+}
+
+module "gke" {
+  source                     = "terraform-google-modules/kubernetes-engine/google"
+  project_id                 = var.gcp_project
+  name                       = var.gke_cluster
+  region                     = var.region
+  regional                   = var.gke_regional
+  zones                      = var.gke_zone
+  network                    = var.gke_network
+  subnetwork                 = var.gke_subnetwork
+  ip_range_pods              = ""
+  ip_range_services          = ""
+  http_load_balancing        = false
+  network_policy             = false
+  horizontal_pod_autoscaling = true
+  filestore_csi_driver       = false
+
+  node_pools = [
+    {
+      name                      = var.gke_nodepool
+      machine_type              = "e2-medium"
+      #node_locations            = "us-central1-b,us-central1-c" -Already specified in the cluster itself-
+      min_count                 = 1
+      max_count                 = 3
+      local_ssd_count           = 0
+      spot                      = false
+      disk_size_gb              = 100
+      disk_type                 = "pd-standard"
+      image_type                = "COS_CONTAINERD"
+      enable_gcfs               = false
+      enable_gvnic              = false
+      auto_repair               = true
+      auto_upgrade              = true
+      service_account           = var.gke_service_account
+      preemptible               = true
+      initial_node_count        = 1
+    },
+  ]
+
+  node_pools_oauth_scopes = {
+    all = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+
+  node_pools_labels = {
+    all = {}
+
+    default-node-pool = {
+      default-node-pool = true
+    }
+  }
+
+  node_pools_metadata = {
+    all = {}
+
+    default-node-pool = {
+      node-pool-metadata-custom-value = "my-node-pool"
+    }
+  }
+
+  node_pools_taints = {
+    all = []
+
+    default-node-pool = [
+      {
+        key    = "default-node-pool"
+        value  = true
+        effect = "PREFER_NO_SCHEDULE"
+      },
+    ]
+  }
+
+  node_pools_tags = {
+    all = []
+
+    default-node-pool = [
+      "default-node-pool",
+    ]
+  }
+}
